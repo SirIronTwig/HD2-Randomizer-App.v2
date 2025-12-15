9321688956800cde35434cab355e9de0d8eee9c6
abbatrombone <redggreen@gmail.com>
2025-12-15 17:13:44 -0600

    Made Rules Tab

---
+++ src/main/java/me/abbatrombone/traz/Panels/RulesPanel.java
@@ -0,0 +1,43 @@
+package me.abbatrombone.traz.Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RulesPanel {
+
+    private final JPanel panel = new JPanel();
+    private JScrollPane jScrollPane = new JScrollPane(panel);
+
+    public RulesPanel(){
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setPreferredSize(new Dimension(1022, 460));
+
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setMargin(new Insets(5,5,5,5));
+
+        textArea.setText("""
+                1) Once given the order by General Brash that is you must use that load out to the best of your ability.
+                    1a) If missing the correct armor do the same armor rating (light, med, heavy) and pick your passive.
+                    1b) If missing the correct weapon use the same type of weapon (AR, shotgun, etc) with the same level armor pen if possible.
+                    1c) If missing the throwable use a throwable from the same category.
+                    1d) If missing a stratagem use one of the same color.
+                2) Use the highest number booster, should that one be taken use the next highest.
+                3) Click Challenges to make it even harder
+                4) Win for Democracy!
+                """);
+        panel.add(textArea);
+
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+    public JScrollPane getjScrollPane() {
+        return jScrollPane;
+    }
+
+}
